b71825c92d436fa2d408bf7939dda057a488e1f8
MalikHeron <malik.heron2001@gmail.com>
2022-10-19 20:50:02 -0500

    updated class

--- Client/src/view/components/DatePicker.java
+++ Client/src/view/components/DatePicker.java
@@ -6,11 +6,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DatePicker extends JPanel {
-    private final JComboBox<Integer> daySelect;
-    private final JComboBox<String> monthSelect;
-    private final JComboBox<Integer> yearSelect;
+
+    private final JComboBox<Integer> dayBox;
+    private final JComboBox<String> monthBox;
+    private final JComboBox<Integer> yearBox;
     private final Integer[] days = new Integer[31];
-    private final String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    private final String[] months = {
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
     private final Integer[] years = new Integer[100];
 
     public DatePicker() {
@@ -28,32 +32,32 @@ public class DatePicker extends JPanel {
 
         Dimension boxSize = new Dimension(70, 30);
 
-        daySelect = new JComboBox<>(days);
-        daySelect.setPreferredSize(boxSize);
-        daySelect.setBorder(new RoundedBorder(7));
-        daySelect.setFocusable(false);
+        dayBox = new JComboBox<>(days);
+        dayBox.setPreferredSize(boxSize);
+        dayBox.setBorder(new RoundedBorder(7));
+        dayBox.setFocusable(false);
 
-        monthSelect = new JComboBox<>(months);
-        monthSelect.setPreferredSize(boxSize);
-        monthSelect.setBorder(new RoundedBorder(7));
-        monthSelect.setFocusable(false);
+        monthBox = new JComboBox<>(months);
+        monthBox.setPreferredSize(boxSize);
+        monthBox.setBorder(new RoundedBorder(7));
+        monthBox.setFocusable(false);
 
-        yearSelect = new JComboBox<>(years);
-        yearSelect.setPreferredSize(boxSize);
-        yearSelect.setBorder(new RoundedBorder(7));
-        yearSelect.setFocusable(false);
-        yearSelect.setSelectedIndex(40);
+        yearBox = new JComboBox<>(years);
+        yearBox.setPreferredSize(boxSize);
+        yearBox.setBorder(new RoundedBorder(7));
+        yearBox.setFocusable(false);
+        yearBox.setSelectedIndex(40);
 
-        add(daySelect);
-        add(monthSelect);
-        add(yearSelect);
+        add(dayBox);
+        add(monthBox);
+        add(yearBox);
     }
 
-    // return the date in YY/MM/DD format
+    // return the date in MM DD, YYYY format
     public String getSelectedDate() {
-        return months[monthSelect.getSelectedIndex()] + " " +
-                days[daySelect.getSelectedIndex()] + ", " +
-                years[yearSelect.getSelectedIndex()];
+        return months[monthBox.getSelectedIndex()] + " " +
+                days[dayBox.getSelectedIndex()] + ", " +
+                years[yearBox.getSelectedIndex()];
     }
 
 }
