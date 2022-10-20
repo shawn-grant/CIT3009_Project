7461ede4ffde97c450a4656ec826fc0a579bbe69
MalikHeron <malik.heron2001@gmail.com>
2022-10-19 21:21:02 -0500

    updated class

--- Client/src/view/InvoiceScreen.java
+++ Client/src/view/InvoiceScreen.java
@@ -1,31 +1,34 @@
 package view;
 
+import view.dialogs.inventory.InventoryInsertDialog;
+import view.dialogs.inventory.InventoryRemoveDialog;
+import view.dialogs.inventory.InventorySearchDialog;
+import view.dialogs.inventory.InventoryUpdateDialog;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class InvoiceScreen extends BaseScreen {
+public class InvoiceScreen extends BaseScreen implements ActionListener {
     private final String[] tableHeaders = {
-        "ID",
-        "First Name",
-        "Last Name",
-        "DOB",
-        "Email",
-        "Phone",
-        "Address",
-        "Membership Date",
-        "Expiry Date",
+        "Invoice Number",
+        "Billing Date",
+        "Item Name",
+        "Quantity",
+        "Employee ID",
+        "Customer ID"
     };
     private JTable table;
     private DefaultTableModel model;
 
     public InvoiceScreen(){
         super("Invoices");
-
         buttonPanel.setVisible(false);
 
         initializeComponents();
-        // setupListeners();
+        setupListeners();
         setContentView();
         // getData();
     }
@@ -40,5 +43,33 @@ public class InvoiceScreen extends BaseScreen {
     // set main content view
     private void setContentView() {
         setMainContent(new JScrollPane(table));
+    }
+
+    // setup actions for buttons
+    private void setupListeners() {
+        addButton.addActionListener(this);
+        updateButton.addActionListener(this);
+        deleteButton.addActionListener(this);
+        searchButton.addActionListener(this);
+        refreshButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource().equals(addButton)) {
+            //new InventoryInsertDialog();
+        }
+        if (e.getSource().equals(searchButton)) {
+            //new InventorySearchDialog(model);
+        }
+        if (e.getSource().equals(updateButton)) {
+            //new InventoryUpdateDialog();
+        }
+        if (e.getSource().equals(deleteButton)) {
+           // new InventoryRemoveDialog();
+        }
+        if (e.getSource().equals(refreshButton)) {
+        }
     }
 }
