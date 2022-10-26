c821213abbfa12ee83d47487c46422d31e35c7ba
Thorne123 <Torihorne190@gmail.com>
2022-10-26 16:52:57 -0700

    Invoice-ui update

--- Client/src/view/dialogs/invoice/SearchDialog.java
+++ Client/src/view/dialogs/invoice/SearchDialog.java
@@ -1,4 +1,4 @@
-package view.dialogs.invoice;
+/*package view.dialogs.invoice;
 //============================================================================
 //Name        : CheckoutScreen.java
 //Author      : Tyrien Gilpin
@@ -152,4 +152,131 @@ public class SearchDialog extends JDialog implements ActionListener {
         }
     }
     
+}*/
+
+package view.dialogs.invoice;
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import client.Client;
+import models.Invoice;
+import view.components.RoundedBorder;
+
+public class SearchDialog extends JDialog implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	 private final DefaultTableModel model;
+	    private JLabel invoiceCodeLabel;
+	    private JTextField InvoiceField;
+	    private JButton confirmButton;
+	    
+	    
+	    public SearchDialog(DefaultTableModel model) {
+	        this.model = model;
+	        initializeComponents();
+	        addComponentsToWindow();
+	        registerListeners();
+	        setWindowProperties();
+	    }
+
+
+	    private void initializeComponents() {
+	        //Label properties
+	    	invoiceCodeLabel = new JLabel("Invoice Code");
+	    	invoiceCodeLabel.setFont(new Font("arial", Font.BOLD, 14));
+	    	invoiceCodeLabel.setPreferredSize(new Dimension(100, 20));
+
+	        //Field properties
+	    	InvoiceField = new JTextField();
+	    	InvoiceField.setFont(new Font("times new roman", Font.PLAIN, 14));
+	    	InvoiceField.setBorder(new RoundedBorder(8));
+	    	InvoiceField.setPreferredSize(new Dimension(90, 35));
+
+	        //Button properties
+	        confirmButton = new JButton("SEARCH");
+	        confirmButton.setPreferredSize(new Dimension(100, 30));
+	        confirmButton.setForeground(Color.BLUE);
+	        confirmButton.setFont(new Font("arial", Font.BOLD, 14));
+
+	        //Additional properties
+	        confirmButton.setFocusPainted(false);
+	    }
+
+	    private void addComponentsToWindow() {
+	        add(invoiceCodeLabel);
+	        add(InvoiceField);
+	        add(confirmButton);
+	    }
+
+	    private void setWindowProperties() {
+	        setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
+	        setTitle("Search Invoice");
+	        setSize(350, 90);
+	        setLocationRelativeTo(null);
+	        setResizable(false);
+	        setModal(true);
+	        setVisible(true);
+	    }
+
+	    private void registerListeners() {
+	        confirmButton.addActionListener(this);
+	    }
+
+	    private boolean validateFields() {
+	        return !(InvoiceField.getText().isEmpty());
+	    }
+
+	    private void setInvoice(Invoice invoice) {
+	        int count = 0;
+	        int rowCount = model.getRowCount();
+	        int counter = 0;
+
+	        while (counter < rowCount) {
+	            model.removeRow(count);
+	            counter++;
+	        }
+
+	        model.insertRow(count, new Object[]{
+	        		invoice.getInvoice_number(),
+	                invoice.getBillingDate(),
+	               // invoice.getItemName(),
+	                //invoice.getTotalQuantity(),
+	                //invoice.getTotalCost(),
+	                //invoice.getEmployee(),
+	                //invoice.getCustomer()
+	        		
+	        });
+	    }
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+	        if (e.getSource().equals(confirmButton)) {
+	            if (validateFields()) {
+	                Client client = new Client();
+	                client.sendAction("Find Invoice");
+	              //  client.sendInvoiceNumber(InvoiceField.getText());
+	                Invoice invoice = client.receiveFindInvoiceResponse();
+	                client.closeConnections();
+	                if (invoice != null) {
+	                    setInvoice(invoice);
+	                    dispose();
+	                }
+	            }
+	        }
+	    }
+   
+
 }
+
--- Client/src/view/screens/InvoiceScreen.java
+++ Client/src/view/screens/InvoiceScreen.java
@@ -1,4 +1,4 @@
-package view.screens;
+/*package view.screens;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -68,4 +68,193 @@ public class InvoiceScreen extends BaseScreen implements ActionListener {
         if (e.getSource().equals(refreshButton)) {
         }
     }
+}*/
+
+
+package view.screens;
+
+//import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.table.DefaultTableModel;
+
+//import view.components.RoundedBorder;
+import view.dialogs.invoice.SearchDialog;
+
+//import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InvoiceScreen extends BaseScreen implements ActionListener {
+
+  
+	private static final long serialVersionUID = 1L;
+	private final GridBagConstraints gbc = new GridBagConstraints();
+	private JTextArea textArea;
+	private JButton viewButton;
+	private JPanel bottomPanel,mainContent, panTop;
+	
+	private final String[] tableHeaders = {
+        "Invoice Number",
+        "Billing Date",
+        "Item Name",
+        "Total Quantity",
+        "Total Cost",
+        "Employee ID",
+        "Customer ID"
+    };
+    private JTable table;
+    private DefaultTableModel model;
+
+    public InvoiceScreen() {
+        super("Invoices");
+        buttonPanel.setVisible(false);
+
+        initializeComponents();
+        setupListeners();
+        setContentView();
+        addComponentsToPanels();
+        addPanelsToWindow();
+        setUpListeners();
+        // getData();
+    }
+
+    private void initializeComponents() {
+    	textArea = new JTextArea(
+    			"Jan Wholesale & Retail " +
+    			"121 Old Hope Road \n"+
+    			"Kingston 6, Jamaica  " +
+    			"Tel: 993-3454"
+    	);
+    	textArea.setFont(new Font("arial", Font.BOLD, 14));
+    	textArea.setLineWrap(true);
+    	textArea.setSize(175, 50);
+    	textArea.setForeground(Color.WHITE);
+    	textArea.setBackground(new Color(0, 100, 205));
+    	textArea.setLayout(new FlowLayout(FlowLayout.RIGHT));
+    	textArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+    	textArea.setWrapStyleWord(true);
+	
+        model = new DefaultTableModel(tableHeaders, 0);
+        table = new JTable(model);
+        table.setDefaultEditor(Object.class, null); //Set to not editable
+        table.setAutoCreateRowSorter(true); //Enable sorting by columns
+        
+        viewButton= new JButton("View");
+        viewButton.setSize(25, 20);
+        viewButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        viewButton.setFont(new Font("arial", Font.PLAIN, 15));
+        viewButton.setFocusPainted(false);
+      //viewButton.setBorder(new RoundedBorder(8));
+        //viewButton.setPreferredSize(new Dimension(25, 25));
+        
+        searchButton.setAlignmentX(LEFT_ALIGNMENT);
+        refreshButton.setAlignmentX(LEFT_ALIGNMENT);
+        
+       /* buttonPanel = new JPanel();
+        buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        buttonPanel.setPreferredSize(new Dimension(0,20));
+        buttonPanel.setBackground(Color.GREEN);
+        */
+        panTop= new JPanel(new GridBagLayout());
+        panTop.setPreferredSize(new Dimension(35,20));
+        panTop.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panTop.setBackground(Color.GREEN);
+        
+        //buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        mainContent = new JPanel(new GridLayout(0, 1, 0, 70));
+        
+        bottomPanel = new JPanel(new GridLayout(3,3,1,0));
+        bottomPanel.setBackground(Color.red);
+    }
+
+    public void addComponentsToPanels() {
+    	gbc.insets = new Insets(30, 20, 30, 20);
+    	gbc.gridwidth = 1;
+    	gbc.gridheight = 4;
+    	gbc.weightx = 0.2;
+        gbc.fill = GridBagConstraints.BOTH;
+    	gbc.gridx = 1;
+        gbc.gridy = 3;
+        panTop.add(textArea, gbc);
+        
+        gbc.gridwidth = 1;
+    	gbc.gridheight = 3;
+        gbc.gridx = 3;
+        gbc.gridy = 3;
+        gbc.fill = GridBagConstraints.BOTH;
+        panTop.add(searchButton, gbc);
+
+        gbc.gridx = 4;
+        gbc.gridy = 3;
+        panTop.add(refreshButton, gbc);
+        
+    	bottomPanel.add(viewButton);
+    }
+    
+    public void addPanelsToWindow(){
+    	//this.add(Box.createRigidArea(new Dimension(0, 20)));
+    	this.add(panTop);
+    	//this.add(buttonPanel);
+    	this.add(mainContent);
+    	this.add(bottomPanel);	
+    }
+    public void setMainContent(Component content) {
+        mainContent.add(content);
+    }
+    // set main content view
+   private void setContentView() {
+        setMainContent(new JScrollPane(table));
+    }
+    // setup actions for buttons
+    private void setupListeners() {
+        addButton.addActionListener(this);
+        updateButton.addActionListener(this);
+        deleteButton.addActionListener(this);
+        searchButton.addActionListener(this);
+        refreshButton.addActionListener(this);
+    }
+    private void setUpListeners() {
+    	
+    	viewButton.addActionListener(this);
+    }
+    
+    @Override
+    
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource().equals(addButton)) {
+        	//addButton.setVisible(false);
+            //new InsertDialog();
+        }
+        if (e.getSource().equals(searchButton)) {
+            new SearchDialog(model);
+        }
+        if (e.getSource().equals(updateButton)) {
+        	//updateButton.setVisible(false);
+        	//new UpdateDialog();
+        }
+        if (e.getSource().equals(deleteButton)) {
+        	//deleteButton.setVisible(false);
+        }
+        if (e.getSource().equals(refreshButton)) {
+        }
+        if (e.getSource().equals(viewButton)) {
+        	//new ViewDialog();
+        }
+        
+    }
+    
 }
