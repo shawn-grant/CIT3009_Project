4dad5da29c5d46d1d07fd23afb10ab062173d434
MalikHeron <malik.heron2001@gmail.com>
2022-11-07 12:06:20 -0500

    changes to libraries

--- Server/src/server/Driver.java
+++ Server/src/server/Driver.java
@@ -8,8 +8,8 @@ public class Driver {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
-                 UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException ex) {
             ex.printStackTrace();
         }
         new Server();
